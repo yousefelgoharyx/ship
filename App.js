f5ed99c30a525459c82e682a0c0e2657646f07ef
Yousef M. El-Gohary <yousefelgoharyx@gmail.com>
2021-08-10 00:30:12 +0200

    add font arabic

--- App.js
+++ App.js
@@ -1,16 +1,23 @@
-import { StatusBar } from "expo-status-bar";
 import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, View, StatusBar, Button} from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
 import useFont from "./src/hooks/useFont";
 const Stack = createStackNavigator();
 
+function StyledText({children, weight, style}) {
+  let fontWeight = 'Cairo_400Regular';
+  if (weight === 700 || weight === 'bold') fontWeight = 'Cairo_700Bold'
+  return (
+    <Text style={{...style, fontWeight}}>{children}</Text>
+  )
+}
+
 function Home() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <StyledText>اهلا صفحة 1</StyledText>
+      <Button title="الي صفحة 1" onPress={() => navigation.navigate('home2')} />
     </View>
   );
 }
@@ -18,8 +25,8 @@ function Home() {
 function Home2() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <StyledText>اهلا صفحة 1</StyledText>
+      <Button title="الي صفحة 1" onPress={() => navigation.navigate('home2')} />
     </View>
   );
 }
@@ -28,8 +35,8 @@ export default function App() {
   return (
     <NavigationContainer>
       <Stack>
-        <Stack.Screen component={Home} />
-        <Stack.Screen component={Home2} />
+        <Stack.Screen component={Home} name="home" />
+        <Stack.Screen component={Home2} name="home2" />
       </Stack>
     </NavigationContainer>
   );
