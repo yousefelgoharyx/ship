6770ca57c862d25d55cb28c1f358b27ef04db080
Yousef M. El-Gohary <yousefelgoharyx@gmail.com>
2021-08-10 15:13:14 +0200

    add gradient support

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,26 +1,29 @@
-import React from 'react'
-import { TouchableOpacity, StyleSheet } from 'react-native'
-import StyledText from './StyledText'
-
-const Button = ({children}) => {
-    return (
-        <TouchableOpacity style={styles.button} activeOpacity={0.7}>
-            <StyledText style={styles.buttonText} weight="bold">{children}</StyledText>
-        </TouchableOpacity>
-    )
-}
+import React from "react";
+import { TouchableOpacity, StyleSheet } from "react-native";
+import StyledText from "./StyledText";
+import { LinearGradient } from "expo-linear-gradient";
+const Button = ({ children }) => {
+  return (
+      <TouchableOpacity activeOpacity={0.7}>
+          <LinearGradient style={styles.button} colors={["#ff3333", "#b763f2"]} start={[1, 1]} end={[0.1,0.1]}>
+        <StyledText style={styles.buttonText} weight="bold">
+          {children}
+        </StyledText>
+    </LinearGradient>
+      </TouchableOpacity>
+  );
+};
 
 const styles = StyleSheet.create({
-    button: {
-        backgroundColor: "#ff3333",
-        width: "100%",
-        height: 56,
-        alignItems: 'center',
-        justifyContent: 'center',
-        borderRadius: 8
-    },
-    buttonText: {
-        color: "#fff"
-    }
-})
-export default Button
+  button: {
+    width: "100%",
+    height: 56,
+    alignItems: "center",
+    justifyContent: "center",
+    borderRadius: 8,
+  },
+  buttonText: {
+    color: "#fff",
+  },
+});
+export default Button;
