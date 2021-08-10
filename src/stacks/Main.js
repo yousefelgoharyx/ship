ee2c806e4a248896c9e78b26cd200e258301ed41
Yousef M. El-Gohary <yousefelgoharyx@gmail.com>
2021-08-10 18:45:55 +0200

    navbar

--- src/stacks/Main.js
+++ src/stacks/Main.js
@@ -1,15 +1,70 @@
 import React from "react";
 import { createDrawerNavigator } from "@react-navigation/drawer";
 import Home from "../screens/Home";
-import { Image, TouchableOpacity, View } from "react-native";
+import {
+  Image,
+  TouchableOpacity,
+  View,
+  StyleSheet,
+  ScrollView,
+} from "react-native";
 import StyledText from "../components/StyledText";
 import Spacer from "../components/Spacer";
 import { MaterialIcons } from "@expo/vector-icons";
 const Drawer = createDrawerNavigator();
+
+const links = [
+  {
+    route: "Home",
+    icon: "local-shipping",
+    name: "جميع الطلبات",
+  },
+  {
+    route: "Orders",
+    icon: "source",
+    name: "طلباتي",
+  },
+  {
+    route: "Ship",
+    icon: "send",
+    name: "تسليم شحنة",
+  },
+  {
+    route: "Wallet",
+    icon: "credit-card",
+    name: "محفظتي",
+  },
+  {
+    route: "Backs",
+    icon: "swap-horiz",
+    name: "مرتجعة",
+  },
+  {
+    route: "Notis",
+    icon: "notifications",
+    name: "اشعارات",
+  },
+  {
+    route: "Settings",
+    icon: "settings",
+    name: "اعدادات",
+  },
+  {
+    route: "Ticket",
+    icon: "confirmation-number",
+    name: "تذكرة دعم",
+  },
+  {
+    route: "Logout",
+    icon: "logout",
+    name: "خروج",
+  },
+];
 const Main = () => {
   return (
     <Drawer.Navigator
-      drawerContent={({ state }) => {
+      drawerContent={({ state, navigation }) => {
+        console.log("---------------------------------------------------");
         console.log(state);
         return (
           <View>
@@ -38,22 +93,24 @@ const Main = () => {
                 yousefelgoharyx@gmail.com
               </StyledText>
             </View>
-            <View>
-              <TouchableOpacity
-                style={{
-                  flexDirection: "row-reverse",
-                  alignItems: "center",
-                  padding: 16,
-                }}
-              >
-                <MaterialIcons
-                  name="local-activity"
-                  style={{ marginLeft: 8 }}
-                  size={24}
-                />
-                <StyledText>جميع الطلبات</StyledText>
-              </TouchableOpacity>
-            </View>
+            <ScrollView>
+              {links.map((link) => {
+                return (
+                  <>
+                    <Spacer space={8} />
+                    <Link
+                      icon={link.icon}
+                      onPress={() => navigation.navigate(link.route)}
+                      active={state.routeNames[state.index] === link.route}
+                    >
+                      {link.name}
+                    </Link>
+                  </>
+                );
+              })}
+
+           
+            </ScrollView>
           </View>
         );
       }}
@@ -63,8 +120,62 @@ const Main = () => {
       }}
     >
       <Drawer.Screen name="Home" component={Home} />
+      <Drawer.Screen name="Orders" component={Home} />
     </Drawer.Navigator>
   );
 };
+
+const Link = (props) => {
+  return (
+    <TouchableOpacity
+      activeOpacity={0.7}
+      onPress={props.onPress}
+      style={
+        props.active
+          ? [styles.link, { backgroundColor: "#ff333320" }]
+          : styles.link
+      }
+    >
+      <MaterialIcons
+        name={props.icon}
+        style={
+          props.active
+            ? [styles.linkIcon, { color: "#ff3333" }]
+            : styles.linkIcon
+        }
+        size={24}
+      />
+      <StyledText
+        style={
+          props.active
+            ? { ...styles.linkText, color: "#ff3333" }
+            : styles.linkText
+        }
+      >
+        {props.children}
+      </StyledText>
+    </TouchableOpacity>
+  );
+};
+
+const styles = StyleSheet.create({
+  link: {
+    flexDirection: "row-reverse",
+    alignItems: "center",
+    padding: 8,
+    marginHorizontal: 8,
+    backgroundColor: "#fff",
+    borderRadius: 8,
+  },
+
+  linkIcon: {
+    marginLeft: 8,
+    color: "#999",
+  },
+
+  linkText: {
+    color: "#999",
+  },
+});
 
 export default Main;
